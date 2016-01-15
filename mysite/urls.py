a5f4611dcf9385847edc794ec87001fc6d2f877c
wojiaohgl <wojiaohgl@126.com>
2016-01-15 16:21:44 +0800

    modified:   .gitignore 	modified:   mysite/settings.pyc 	modified:   mysite/urls.py 	statics/

--- mysite/urls.py
+++ mysite/urls.py
@@ -28,4 +28,5 @@ urlpatterns = [
     url(r'^siteComment/','blog.views.siteComment'),
     url(r'^toDoList/',include('ToDoList.urls')),
 ]
-urlpatterns+=static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+#urlpatterns+=static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+#urlpatterns+=static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
